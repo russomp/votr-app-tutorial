0675f90b3d1457b4a27dca077813a7dd2862c668
russomp <russomp@usc.edu>
2017-08-15 01:15:56 -0700

    Initial app setup

---
+++ README.md
@@ -0,0 +1,3 @@
+#Votr App Tutorial
+==================
+A small flask app for tracking online polls built as a first exploration of Flask, ReactJS and Celery.
---
+++ votr/votr.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from models import db
+
+votr = Flask(__name__)
+
+votr.config.from_object('config')
+
+db.init_app(votr)
+db.create_all(app=votr)
+
+
+@votr.route('/')
+def home():
+    return 'Hello There!'
+
+if __name__=='__main__':
+    votr.run()
